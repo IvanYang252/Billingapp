efee4bef01654407ea36ba6ff1a3e20537366843
Yifan Yang <y963yang@uwaterloo.ca>
2025-10-03 09:53:43 -0400

    show categories on web

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,27 +1,31 @@
 import { createContext, useEffect, useState} from "react";
 import { readCategories } from "../Service/CategoryService";
 
+// Create a global context (like a shared store for the app)
 export const AppContext = createContext(null);
 
 export const AppContextProvider = (props) => {
 
-
+    // State to store all categories
     const [categories, setCategories] = useState([]);
 
+    // Runs once when the component loads ([] means only on first render)
     useEffect(() => {
         async function loadData(params) {
-            const response = await readCategories();
-            setCategories(response.data)
+            const response = await readCategories(); // Call backend service to get category data
+            setCategories(response.data) // Save data into state
         }
-        loadData
+        loadData(); // Call function
     }, []);
 
 
+    // Values to share with all children components
     const contextValue = {
         categories,
         setCategories
     }
 
+    // Provide shared data (context) to all children
     return <AppContext.Provider value={contextValue}>
         {props.children}
     </AppContext.Provider>
